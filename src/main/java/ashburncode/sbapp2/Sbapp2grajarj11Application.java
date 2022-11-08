c0bd42393d8bb72fecd68c88c17d8d3d0a7215e4
ashburndev <ashburndev@gmail.com>
2022-11-08 08:02:19 -0500

    first commit

---
+++ src/main/java/ashburncode/sbapp2/Sbapp2grajarj11Application.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbapp2grajarj11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbapp2grajarj11Application.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/sbapp2/Sbapp2grajarj11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.sbapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapp2grajarj11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
